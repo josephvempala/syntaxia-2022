c0ba73a6a9cd532167611fc4707b5fef9d9906e4
Joseph Boby <josephvempala@gmail.com>
2021-04-30 15:13:57 +0530

    completed front end changes

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React,{useRef, useEffect} from "react";
+import {useMediaQuery} from 'react-responsive';
 import Carousel from "./CarouselComponent";
 
-import { Button } from "reactstrap";
 export default function HomeComponent() {
+  const scrollRef = useRef(null);
+  const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
+  useEffect(()=>{
+    if(isMobile)
+    {
+      scrollRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest',
+      });
+    }
+  },[]);
   return (
-    <div className="container my-3">
+    <div ref={scrollRef} className="container my-3">
       <Carousel />
     </div>
   );
