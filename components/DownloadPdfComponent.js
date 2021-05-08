06890f3834598bb0a1432dd105495603cd7f502a
Punith <punith@pop-os.localdomain>
2021-05-08 13:41:30 +0530

    s3-url

--- components/DownloadPdfComponent.js
+++ components/DownloadPdfComponent.js
@@ -1,8 +1,12 @@
 import React from "react";
-import Pdf from "./Brochure.pdf";
 export function Download(props) {
   return (
-    <a className={props.className} rel="noreferrer" href={Pdf} target="_blank">
+    <a
+      className={props.className}
+      rel="noreferrer"
+      href="https://syntaxia-2021.s3.us-east-2.amazonaws.com/brochure-syntaxia.pdf"
+      target="_blank"
+    >
       {props.children}
     </a>
   );
