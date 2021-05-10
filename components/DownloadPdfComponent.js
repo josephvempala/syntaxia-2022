56fa7b805b2172b42f4a5f73f019bfb11fdb326b
Punithify <punithsp9@gmail.com>
2021-05-10 09:29:53 +0530

    new url

--- components/DownloadPdfComponent.js
+++ components/DownloadPdfComponent.js
@@ -4,7 +4,7 @@ export function Download(props) {
     <a
       className={props.className}
       rel="noreferrer"
-      href="https://syntaxia-2021.s3.us-east-2.amazonaws.com/brochure-syntaxia.pdf"
+      href="https://syntaxia-2021.s3.us-east-2.amazonaws.com/Official+Brochure+SYNTAXIA+2021.pdf"
       target="_blank"
     >
       {props.children}
